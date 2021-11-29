3b0a3f3137710cd0386896c2d3781198b38c9a91
Grigore Stefan <g_stefan@yahoo.com>
2021-11-29 14:47:35 +0200

    Update

--- build/msvc.install.sub.cmd
+++ build/msvc.install.sub.cmd
@@ -11,8 +11,8 @@ if errorlevel 1 exit 1
 rem ---
 
 if not exist "%INSTALL_PATH_BIN%\" mkdir "%INSTALL_PATH_BIN%"
-xcopy /Y /S /E "temp\wxwidgets\bin\*.exe" "%INSTALL_PATH_BIN%\"
-xcopy /Y /S /E "temp\wxwidgets\bin\*.dll" "%INSTALL_PATH_BIN%\"
+xcopy /Y /S /E "output\bin\*.exe" "%INSTALL_PATH_BIN%\"
+xcopy /Y /S /E "output\bin\*.dll" "%INSTALL_PATH_BIN%\"
 
 rem --- dev
 
@@ -20,7 +20,7 @@ if not exist "%INSTALL_PATH_DEV%\" mkdir "%INSTALL_PATH_DEV%"
 if not exist "%INSTALL_PATH_DEV%\bin" mkdir "%INSTALL_PATH_DEV%\bin"
 if not exist "%INSTALL_PATH_DEV%\include" mkdir "%INSTALL_PATH_DEV%\include"
 if not exist "%INSTALL_PATH_DEV%\lib" mkdir "%INSTALL_PATH_DEV%\lib"
-xcopy /Y /S /E "temp\wxwidgets\bin\*.exe" "%INSTALL_PATH_DEV%\bin\"
-xcopy /Y /S /E "temp\wxwidgets\include\" "%INSTALL_PATH_DEV%\include\"
-if exist temp\wxwidgets\lib\vc_x64_lib\ copy /Y /B "temp\wxwidgets\lib\vc_x64_lib\*.lib" "%INSTALL_PATH_DEV%\lib\*"
-if exist temp\wxwidgets\lib\vc_lib\ copy /Y /B "temp\wxwidgets\lib\vc_lib\*.lib" "%INSTALL_PATH_DEV%\lib\*"
+xcopy /Y /S /E "output\bin\*.exe" "%INSTALL_PATH_DEV%\bin\"
+xcopy /Y /S /E "output\include\" "%INSTALL_PATH_DEV%\include\"
+if exist output\lib\vc_x64_lib\ copy /Y /B "output\lib\vc_x64_lib\*.lib" "%INSTALL_PATH_DEV%\lib\*"
+if exist output\lib\vc_lib\ copy /Y /B "output\lib\vc_lib\*.lib" "%INSTALL_PATH_DEV%\lib\*"
